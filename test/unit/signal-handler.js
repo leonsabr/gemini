3821868981dd17d2a6007d2de1b7a5d79515c720
Anton Usmansky <cody0@yandex-team.ru>
2015-12-02 17:08:28 +0300

    fixup

--- test/unit/signal-handler.js
+++ test/unit/signal-handler.js
@@ -4,23 +4,23 @@ var q = require('q'),
     signalHandler = require('../../lib/signal-handler');
 
 describe('signalHandler', function() {
-        var sandbox = sinon.sandbox.create(),
-            defer, onExit;
+    var sandbox = sinon.sandbox.create(),
+        defer, onExit;
 
-        beforeEach(function() {
-            defer = q.defer();
-            onExit = sinon.spy();
-            sandbox.stub(process, 'exit', defer.resolve.bind(defer));
-            signalHandler.on('exit', onExit);
-        });
+    beforeEach(function() {
+        defer = q.defer();
+        onExit = sinon.spy();
+        sandbox.stub(process, 'exit', defer.resolve.bind(defer));
+        signalHandler.on('exit', onExit);
+    });
 
-        afterEach(function() {
-            sandbox.restore();
-        });
+    afterEach(function() {
+        sandbox.restore();
+    });
 
-        // Can't test SIGINT and SIGTERM because these signals will kill test process
-        it('should send `exit` on SIGHUP, SIGINT and SIGTERM', function() {
-            process.emit('SIGHUP');
-            assert.calledOnce(onExit);
-        });
+    // Can't test SIGINT and SIGTERM because these signals will kill test process
+    it('should send `exit` on SIGHUP, SIGINT and SIGTERM', function() {
+        process.emit('SIGHUP');
+        assert.calledOnce(onExit);
+    });
 });
